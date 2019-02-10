b13c9edbfd22b4372cd1d70fa0878dc325c7d454
shanmathi <shanmathithavaselvan@gmail.com>
2019-02-10 11:01:49 +0530

    pawan kalyan movies added

---
+++ test.py
@@ -0,0 +1,12 @@
+
+def call_vijay():
+    print("vijay")
+
+#call_vijay()
+ 
+def get_pawan_kalyan_movies():
+   #print("jalsa")
+    k_list = ["kushi", "agnyaathavaasi"]
+    print(k_list)
+
+get_pawan_kalyan_movies()
