886673f9eb22d0522c31e05d7050d5e901125905
shanmathi <shanmathithavaselvan@gmail.com>
2019-02-16 21:47:45 +0530

    gyf:comment

---
+++ task.py/dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+def merge(dict1, dict2):
+    return(dict2 .update(dict1))
+
+    
+dict1 ={'a': 10 , 'b': 8}
+
+dict2 = {'d' : 6, 'c' : 4}
+    
+print (merge(dict1, dict2))
+
+print (dict2)
